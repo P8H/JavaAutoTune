f3ce1c50f0b32ab54605a042089e871606702bb8
Kevin Rojczyk <kevin.roj@p8h.de>
2017-05-13 18:20:50 +0200

    add nominal parameter tuning

---
+++ src/main/java/NominalParameter.java
@@ -0,0 +1,12 @@
+import java.lang.annotation.*;
+
+/**
+ * Created by KevinRoj on 26.04.17.
+ */
+@Target(ElementType.FIELD)
+@Inherited
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NominalParameter {
+    String[] values();
+}
