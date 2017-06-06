8f542dd4310a988f3c4852a4f27978188a6548f6
Kevin Rojczyk <kevin.roj@p8h.de>
2017-06-06 22:53:02 +0200

    prepare test for final benchmark

--- src/test/java/TunePolynomialFunctionTest.java
+++ src/test/java/TunePolynomialFunctionTest.java
@@ -12,24 +12,25 @@ import java.util.Arrays;
 
 class TunePolynomialFunctionTest {
 
-    @TuneableParameters(initRandomSearch = 5, autoTimeMeasure = true)
+    @TuneableParameters(autoTimeMeasure = true, cacheNextPoints = 1)
     public class VariableXYZ implements Serializable {
         static final long serialVersionUID = 421L;
-        @NumericParameter(min=0, max=100)
-        double x  = 1;
+        @NumericParameter(max = 100)
+        public double x = 1;
         @NumericParameter(min=1, max=100)
-        double y = 1;
+        public double y = 1;
         @NumericParameter(min=1, max=100)
-        double z = 1;
+        public double z = 1;
     }
 
     @org.junit.jupiter.api.Test
-    double simpleProductTest() throws IllegalAccessException {
+    double T() throws IllegalAccessException {
         AutoTune<VariableXYZ> tuner = new AutoTuneDefault(new VariableXYZ());
 
         for (int i = 0; i < 30; i++) {
             VariableXYZ cfg = tuner.start().getConfig();
             double value = cfg.x*cfg.y*cfg.z;
+            tuner.addCost(value);
             tuner.end();
             System.out.printf("Finished iteration %d with value %f \n", i, value);
         }
@@ -48,6 +49,7 @@ class TunePolynomialFunctionTest {
         for (int i = 0; i < 30; i++) {
             VariableXYZ cfg = tuner.start().getConfig();
             double value = Math.sqrt(cfg.z)*cfg.x + 999/(cfg.y+1);
+            tuner.addCost(value);
             tuner.end();
             System.out.printf("Finished iteration %d with value %f \n", i, value);
         }
@@ -63,41 +65,41 @@ class TunePolynomialFunctionTest {
     public class ManyVariables implements Serializable {
         static final long serialVersionUID = 421L;
         @NumericParameter(min=0, max=100)
-        double x  = 1;
+        public double x = 1;
         @NumericParameter(min=1, max=100)
-        double y = 1;
+        public double y = 1;
         @NumericParameter(min=1, max=100)
-        double z = 1;
+        public double z = 1;
         @NumericParameter(min=0, max=50)
-        double x2  = 11;
+        public double x2 = 11;
         @NumericParameter(min=1, max=100)
-        double y2 = 11;
+        public double y2 = 11;
         @NumericParameter(min=1, max=100)
-        double z2 = 11;
+        public double z2 = 11;
         @NumericParameter(min=0, max=42)
-        double x3  = 1;
+        public double x3 = 1;
         @NumericParameter(min=1, max=100)
-        double y3 = 1;
+        public double y3 = 1;
         @NumericParameter(min=1, max=1000)
-        double z3 = 1;
+        public double z3 = 1;
         @NumericParameter(min=0, max=100)
-        double x4  = 1;
+        public double x4 = 1;
         @NumericParameter(min=1, max=100)
-        double y4 = 1;
+        public double y4 = 1;
         @NumericParameter(min=1, max=10000)
-        double z4 = 1;
+        public double z4 = 1;
         @NumericParameter(min=0, max=100)
-        double x5  = 144;
+        public double x5 = 144;
         @NumericParameter(min=1, max=100)
-        double y5 = 1;
+        public double y5 = 1;
         @NumericParameter(min=1, max=10)
-        double z5 = 1;
+        public double z5 = 1;
         @NumericParameter(min=0.5, max=1.9)
-        double x6  = 1;
+        public double x6 = 1;
         @NumericParameter(min=1, max=100)
-        double y6 = 13;
+        public double y6 = 13;
         @NumericParameter(min=1, max=100)
-        double z6 = 1;
+        public double z6 = 1;
     }
 
     @org.junit.jupiter.api.Test
@@ -112,8 +114,8 @@ class TunePolynomialFunctionTest {
             value += Math.sqrt(cfg.z4)*cfg.x4*cfg.x4 + 30/(cfg.y4+5);
             value += Math.sqrt(cfg.z5) + cfg.x5*cfg.x5 + cfg.y5;
             value += cfg.z6*cfg.x6*cfg.y6;
+            tuner.addCost(value);
             tuner.end();
-            System.out.printf("Finished iteration %d with value %f \n", i, value);
         }
 
         System.out.printf("Best configuration found with result %f \n", tuner.getBestResult());
